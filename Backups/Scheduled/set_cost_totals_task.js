583730e555c86990240e8053926763243322db41
davebarletta <dbarletta@compstyle.io>
2023-04-18 07:50:19 -0400

    Fix arrayObjectIndexOf function

--- Backups/Scheduled/set_cost_totals_task.js
+++ Backups/Scheduled/set_cost_totals_task.js
@@ -226,7 +226,7 @@ function splitArrayIntoChunksOfLen(arr, len) {
 
 function arrayObjectIndexOf(myArray, searchTerm, property) {
     for(var i = 0, len = myArray.length; i < len; i++) {
-        if (myArray[i].property === searchTerm) return i;
+        if (myArray[i][property] === searchTerm) return i;
     }
     return -1;
 }
